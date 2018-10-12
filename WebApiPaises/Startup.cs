cc6766a30632e2d95464963e6e44b0bc9d0805c3
zeusn2018 <nunja.zeus@gmail.com>
2018-10-12 15:51:22 -0500

    GRASP

---
+++ WebApiPaises/Startup.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using WebApiPaises.Models;
+
+namespace WebApiPaises
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("paisDB"));
+            //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().AddJsonOptions(ConfigureJson);
+
+        }
+
+        private void ConfigureJson(MvcJsonOptions obj)
+        {
+            //throw new NotImplementedException();
+            obj.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+        }
+
+
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ApplicationDbContext context)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            //app.UseHttpsRedirection();
+            app.UseMvc();
+            //para que ingrese data de prueba
+            if (!context.Paises.Any())
+            {
+                context.Paises.AddRange(new List<Pais>()
+                {
+                    new Pais(){Nombre = "Venezuela", Provincias = new List<Provincia>(){
+                            new Provincia(){ Nombre = "Caracas"}
+                        }
+                    },
+                    new Pais(){Nombre = "Peru", Provincias = new List<Provincia>(){
+                        new Provincia(){ Nombre = "Lima"},
+                        new Provincia(){ Nombre = "Tacna"}
+                    } },
+                    new Pais(){Nombre = "Argentina"}
+                });
+
+                context.SaveChanges();
+            }
+        }
+
+
+
+        //public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        //{
+        //    if (env.IsDevelopment())
+        //    {
+        //        app.UseDeveloperExceptionPage();
+        //    }
+        //    else
+        //    {
+        //        app.UseHsts();
+        //    }
+
+        //    app.UseHttpsRedirection();
+        //    app.UseMvc();
+
+        //    //para que ingrese data de prueba
+
+        //}
+
+
+
+
+    }
+}
---
+++ readme.md
@@ -0,0 +1,15 @@
+#PATRON GRASP
+##NETCore ZN
+Aplicacion usando el patron GRASP 
+El ejercicio consiste en una api de paises con sus provincias.
+
+General Responasibility Assignment Software Patterns (GRASP)
+	Experto en la Información
+	Creador
+	Controlador
+	Fabricación pura 
+	Alta Cohesión
+	Indirección
+	Bajo acoplamiento
+	Polimorfismo
+	Variaciones protegidas
